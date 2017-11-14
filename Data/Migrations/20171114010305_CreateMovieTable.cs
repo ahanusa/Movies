f8343d999cc530b9c3f98bfcbec0d5f87ef2013c
ahanusa <aaron.hanusa@gmail.com>
2017-11-13 19:16:58 -0600

    add create movie migration and run 'dotnet ef database update'

--- Data/Migrations/20171114010305_CreateMovieTable.cs
+++ Data/Migrations/20171114010305_CreateMovieTable.cs
@@ -8,14 +8,6 @@ namespace Movies.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_AspNetUserRoles_UserId",
-                table: "AspNetUserRoles");
-
-            migrationBuilder.DropIndex(
-                name: "RoleNameIndex",
-                table: "AspNetRoles");
-
             migrationBuilder.CreateTable(
                 name: "Movies",
                 columns: table => new
@@ -29,44 +21,13 @@ namespace Movies.Data.Migrations
                 {
                     table.PrimaryKey("PK_Movies", x => x.Id);
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "RoleNameIndex",
-                table: "AspNetRoles",
-                column: "NormalizedName",
-                unique: true);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
-                table: "AspNetUserTokens",
-                column: "UserId",
-                principalTable: "AspNetUsers",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
-                table: "AspNetUserTokens");
-
             migrationBuilder.DropTable(
                 name: "Movies");
 
-            migrationBuilder.DropIndex(
-                name: "RoleNameIndex",
-                table: "AspNetRoles");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_AspNetUserRoles_UserId",
-                table: "AspNetUserRoles",
-                column: "UserId");
-
-            migrationBuilder.CreateIndex(
-                name: "RoleNameIndex",
-                table: "AspNetRoles",
-                column: "NormalizedName");
         }
     }
 }
