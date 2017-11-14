764865eeba1d0ff2e7650a7fb94f7567bffed101
ahanusa <aaron.hanusa@gmail.com>
2017-11-13 18:14:12 -0600

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Movies.Models {
+  public class Movie
+  {
+    public long Id { get; set; }
+    public string ImageUrl { get; set; }
+    public string Title { get; set; }
+  }
+}
