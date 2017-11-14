855d5d78f909cc74f103b16cdd39a71ab609f957
ahanusa <aaron.hanusa@gmail.com>
2017-11-13 18:30:23 -0600

    add MoviesDbContext

---
+++ Data/MoviesDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Movies.Models
+{
+  public class MoviesDbContext : DbContext
+  {
+    public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movies { get; set; }
+  }
+}
