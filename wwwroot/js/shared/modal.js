1f558d4d44dfc2c39d1de52f6a2e949615a07061
ahanusa <aaron.hanusa@gmail.com>
2017-11-18 06:56:21 -0600

    general cleanup and removal of jquery and bootstrap

--- wwwroot/js/shared/modal.js
+++ wwwroot/js/shared/modal.js
@@ -1,21 +1,17 @@
 Vue.component('modal', {
   template: `<transition name="modal">
-  <div class="modal-mask" v-show="show">
+    <div class="modal-mask" v-show="show">
       <div class="modal-container">
           <div class="modal-header">
               <h3>Are you sure?</h3>
           </div>
           <div class="modal-footer">
-              <button class="default-button" @click="close(true)">
-                  YES
-              </button>
-              <button class="default-button" @click="close(false)">
-                  NO
-              </button>
+              <button class="default-button" @click="close(true)">YES</button>
+              <button class="default-button" @click="close(false)">NO</button>
           </div>
       </div>
-  </div>
-</transition>`,
+    </div>
+  </transition>`,
   props: ['show'],
   methods: {
     close: function (accept) {
